757a5bfa5e9f5ffb02a770a8e00f3ca05bf8700d
Pranjal Jain <pranjaljain275@gmail.com>
2023-02-08 16:09:46 +0530

    updated skills

---
+++ script.js
@@ -0,0 +1,8 @@
+function resumeTab1() {
+    window.open(
+   "https://drive.google.com/file/d/1OrNImyNk2xNE_vsnyQ4a8-c2Mq-uT0Xi/view", "_blank");
+}
+function resumeTab2() {
+    window.open(
+   "https://drive.google.com/file/d/1OrNImyNk2xNE_vsnyQ4a8-c2Mq-uT0Xi/view", "_blank");
+}
