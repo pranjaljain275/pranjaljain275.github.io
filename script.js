c16fe6a6bb98a4cac00bb2e9ee92b309a93d4929
Pranjal Jain <pranjaljain275@gmail.com>
2023-02-13 20:55:47 +0530

    updated

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-function resumeTab1() {
-  window.open(
-    "https://drive.google.com/file/d/1OrNImyNk2xNE_vsnyQ4a8-c2Mq-uT0Xi/view",
-    "_blank"
-  );
-}
-function resumeTab2() {
+function resumeTab() {
   window.open(
     "https://drive.google.com/file/d/1OrNImyNk2xNE_vsnyQ4a8-c2Mq-uT0Xi/view",
     "_blank"
