1eaa81964151c583c792968cab8e780bcead6c12
Pranjal Jain <pranjaljain275@gmail.com>
2023-02-21 13:12:17 +0530

    updated

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // RESUME BUTTON
 function resumeTab1() {
-  window.open(
+  location.assign(
     "https://drive.google.com/file/d/1OrNImyNk2xNE_vsnyQ4a8-c2Mq-uT0Xi/view",
     "_blank"
   );
 }
 function resumeTab2() {
-  window.open(
+  location.assign(
     "https://drive.google.com/file/d/1OrNImyNk2xNE_vsnyQ4a8-c2Mq-uT0Xi/view",
     "_blank"
   );
